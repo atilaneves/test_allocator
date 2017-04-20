5efe74407c754982b0c30b64dee67ffc4a1a31ef
Atila Neves <aalvesne@cisco.com>
2017-04-20 21:59:45 +0100

    TestAllocator is @nogc @safe

--- source/test_allocator/package.d
+++ source/test_allocator/package.d
@@ -19,32 +19,71 @@ struct TestAllocator {
 
     enum uint alignment = platformAlignment;
 
-    void[] allocate(size_t numBytes) {
+    void[] allocate(size_t numBytes) @safe @nogc {
+        import std.experimental.allocator: makeArray, expandArray;
+
+        static const exception = new Exception("Allocation failed");
+
         ++_numAllocations;
+
         auto ret = allocator.allocate(numBytes);
-        _allocations ~= ByteRange(ret.ptr, ret.length);
+        if(numBytes > 0 && ret.length == 0)
+            throw exception;
+
+        auto newEntry = ByteRange(&ret[0], ret.length);
+
+        if(_allocations is null)
+            _allocations = allocator.makeArray(1, newEntry);
+        else
+            () @trusted { allocator.expandArray(_allocations, 1, newEntry); }();
+
         return ret;
     }
 
-    bool deallocate(void[] bytes) {
+    bool deallocate(void[] bytes) @trusted @nogc nothrow {
         import std.algorithm: remove, canFind;
-        import std.conv: text;
+        import core.stdc.stdio: sprintf;
 
         bool pred(ByteRange other) { return other.ptr == bytes.ptr && other.length == bytes.length; }
 
-        assert(_allocations.canFind!pred,
-                text("Unknown deallocate byte range. Ptr: ", bytes.ptr, " length: ", bytes.length,
-                     " allocations: ", _allocations));
+        static char[1024] buffer;
+
+        if(!_allocations.canFind!pred) {
+            auto index = sprintf(&buffer[0],
+                                 "Unknown deallocate byte range. Ptr: %p, length: %ld, allocations:\n",
+                                 &bytes[0], bytes.length);
+            index = printAllocations(buffer, index);
+            assert(false, buffer[0 .. index]);
+        }
         _allocations = _allocations.remove!pred;
-        return allocator.deallocate(bytes);
+        return () @trusted { return allocator.deallocate(bytes); }();
     }
 
-    auto numAllocations() @safe pure nothrow const {
+    auto numAllocations() @safe @nogc pure nothrow const {
         return _numAllocations;
     }
 
-    ~this() {
-        import std.conv: text;
-        assert(!_allocations.length, text("Memory leak in TestAllocator. Allocations: ", _allocations));
+    ~this() @trusted @nogc nothrow {
+
+        static char[1024] buffer;
+
+        if(_allocations.length) {
+            import core.stdc.stdio: sprintf;
+            auto index = sprintf(&buffer[0], "Memory leak in TestAllocator. Allocations:\n");
+            index = printAllocations(buffer, index);
+            assert(false, buffer[0 .. index]);
+        }
+    }
+
+    int printAllocations(int N)(ref char[N] buffer, int index = 0) @trusted @nogc const nothrow {
+        import core.stdc.stdio: sprintf;
+        index += sprintf(&buffer[index], "[");
+        foreach(ref allocation; _allocations) {
+            index += sprintf(&buffer[index], "ByteRange(%p, %ld), ",
+                             allocation.ptr, allocation.length);
+        }
+
+        index += sprintf(&buffer[index], "]");
+        return index;
     }
 }
